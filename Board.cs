1eb10599aec001b9754c2024affc3bcc83f80ef9
rickhodder <rickhodder@msn.com>
2021-06-16 22:26:47 -0400

    Row of Xs, X wins

--- Board.cs
+++ Board.cs
@@ -2,11 +2,19 @@ namespace TicTakToeKata
 {
     public class Board
     {
-        private int[,] _cells = new int[3,3] ;
+        private int[,] _cells = new int[3, 3];
 
         public int Evaluate()
         {
+            if(_cells[0,0]==1 && _cells[0,1]==1 && _cells[0,2]==1)
+                return 1;
+                
             return -1;
+        }
+
+        public void SetCell(int row, int column, int value)
+        {
+            _cells[row, column] = value;
         }
     }
 }
--- TicTakToeKataTests.cs
+++ TicTakToeKataTests.cs
@@ -5,12 +5,22 @@ namespace TicTakToeKata
 {
     public class TicTakToeKataTests
     {
+        private Board _sut = new();
+
         [Fact]
         public void EmptyBoard_NotSolved()
         {
-            var board = new Board();
+            Assert.Equal(-1, _sut.Evaluate());
+        }
 
-            Assert.Equal(-1, board.Evaluate());
+        [Fact]
+        public void RowOfXs_XWins()
+        {
+            _sut.SetCell(0,0,1);
+            _sut.SetCell(0,1,1);
+            _sut.SetCell(0,2,1);
+
+            Assert.Equal(1, _sut.Evaluate());
         }
     }
 }
