131ef5ec98f5107072d4709dd562042f3bd3ec1d
rickhodder <rickhodder@msn.com>
2021-06-18 22:46:36 -0400

    reduce cyclomatic complexity

--- Board.cs
+++ Board.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 
 namespace TicTakToeKata
@@ -16,13 +15,13 @@ namespace TicTakToeKata
         {
             var result = Unsolved;
 
-            for (int row = 0; row < _cells.GetLength(0); row++)
+            for (var row = 0; row < _cells.GetLength(0); row++)
             {
                 result = CheckRow(row);
                 if (result != Unsolved) return result;
             }
 
-            for (int column = 0; column < _cells.GetLength(1); column++)
+            for (var column = 0; column < _cells.GetLength(1); column++)
             {
                 result = CheckColumn(column);
                 if (result != Unsolved) return result;
@@ -41,16 +40,26 @@ namespace TicTakToeKata
 
         private int CheckDiagonals()
         {
-            if (_cells[0, 0] == X && _cells[1, 1] == X && _cells[2, 2] == X)
+            if (
+                    (_cells[0, 0] == X && 
+                    _cells[1, 1] == X && 
+                    _cells[2, 2] == X) 
+                    ||
+                    (_cells[0, 2] == X &&
+                     _cells[1, 1] == X &&
+                     _cells[2, 0] == X)
+                )
                 return X;
          
-            if (_cells[0, 0] == O && _cells[1, 1] == O && _cells[2, 2] == O)
-                return O;
-
-            if (_cells[0, 2] == X && _cells[1, 1] == X && _cells[2, 0] == X)
-                return X;
-            
-            if (_cells[0, 2] == O && _cells[1, 1] == O && _cells[2, 0] == O)
+            if (
+                    (_cells[0, 0] == O && 
+                    _cells[1, 1] == O && 
+                    _cells[2, 2] == O)
+                    || 
+                    (_cells[0, 2] == O && 
+                    _cells[1, 1] == O && 
+                    _cells[2, 0] == O)
+                )
                 return O;
 
             return Unsolved;
@@ -58,10 +67,14 @@ namespace TicTakToeKata
 
         private int CheckRow(int row)
         {
-            if (_cells[row, 0] == X && _cells[row, 1] == X && _cells[row, 2] == X)
+            if (_cells[row, 0] == X && 
+                _cells[row, 1] == X && 
+                _cells[row, 2] == X)
                 return X;
 
-            if (_cells[row, 0] == O && _cells[row, 1] == O && _cells[row, 2] == O)
+            if (_cells[row, 0] == O && 
+                _cells[row, 1] == O && 
+                _cells[row, 2] == O)
                 return O;
 
             return Unsolved;
@@ -69,10 +82,14 @@ namespace TicTakToeKata
 
         private int CheckColumn(int column)
         {
-            if (_cells[0, column] == X && _cells[1, column] == X && _cells[2, column] == X)
+            if (_cells[0, column] == X && 
+                _cells[1, column] == X && 
+                _cells[2, column] == X)
                 return X;
 
-            if (_cells[0, column] == O && _cells[1, column] == O && _cells[2, column] == O)
+            if (_cells[0, column] == O && 
+                _cells[1, column] == O && 
+                _cells[2, column] == O)
                 return O;
 
             return Unsolved;
